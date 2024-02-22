6c6b5d6b335de19afc80bebd6af60b3b82ddf7ee
Robson Torres Costa <robsoncosta1234567@gmail.com>
2024-02-22 16:47:39 -0300

    Adicionado

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import "dotenv/config";
+import "express-async-errors";
+import "reflect-metadata";
+import express, { json } from "express";
+import cors from "cors";
+import helmet from "helmet";
+import { HandleErrors } from "./middlewares/handleErrors.middleware";
+import { userRouter } from "../src/routes/user.routes";
+
+export const app = express();
+
+app.use(cors());
+
+app.use(helmet());
+
+app.use(json());
+
+app.use("/users", userRouter);
+
+app.use(HandleErrors.execute);
